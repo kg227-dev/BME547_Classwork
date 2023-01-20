9b4a2cc232ad254b922fc713bf5ef62f857354cf
Kush <kg227@duke.edu>
2023-01-20 12:46:03 -0500

    added function to categorize HDL values

--- blood_calculator.py
+++ blood_calculator.py
@@ -4,7 +4,7 @@ def interface():
     while keep_running: 
         print("Options:")
         print("9 - Quit")
-        choice = input("Select an option:")
+        choice = input("Select an option: ")
         if choice == "9":
             keep_running= False 
     print("Program ending")
@@ -13,5 +13,14 @@ def HDL_input():
     HDL_value = input("Enter the HDL result:")
     HDL_value = int(HDL_value)
     return HDL_value
+
+def HDL_analysis(HDL_int):
+    if HDL_int >= 60:
+        answer = "Normal"
+    elif 40 <= HDL_int <60:
+        answer = "Borderline Low"
+    else:
+        answer = "Low"
+    return answer
 
 interface()
