9e457a004d798d39a98863364e9711882797f8a6
Kush <kg227@duke.edu>
2023-01-25 12:19:46 -0500

    modules

---
+++ database.py
@@ -0,0 +1,11 @@
+
+print("This is the database.py file")
+print("Python thinks this is called {}".format(__name__))
+import blood_calculator
+HDL = 55
+HDL_analysis = blood_calculator.HDL_analysis(HDL)
+
+print("The HDL analysis is {}".format(HDL_analysis))
+
+
+
