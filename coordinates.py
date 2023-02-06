70f4be1a3503893aec221e4f0e9b86fc2f328e91
kush <kg227@duke.edu>
2023-02-05 23:02:47 -0500

    updated tests

--- coordinates.py
+++ coordinates.py
@@ -5,11 +5,7 @@ def get_line(p1, p2):
     b = y1-m*x1
     return m,b
 
-def get_y(m,b):
-    x1,y1 = p1
-    x2,y2 = p2
-    m = (y2-y1) / (x2-x1)
-    b = y1 - m*x1
+def get_y(m,b,x):
     y = m*x+b
     return y
 
@@ -17,5 +13,6 @@ if __name__ == "__main__":
     p1 = (7,1)
     p2 = (-3,-4)
     m, b = get_line(p1,p2)
-    y = get_y(m, b)
+    x=3
+    y = get_y(m, b, x)
     print(y)
