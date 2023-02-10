f3bb6367c4003d41f9ceef62e306cbeb7b001fb6
kush <kg227@duke.edu>
2023-02-10 12:21:29 -0500

    converted list in database.py to dictionary

--- database.py
+++ database.py
@@ -1,13 +1,16 @@
-def create_patient_entry(patient_name, patient_mrn, patient_age):
-    new_patient = [patient_name, patient_mrn, patient_age, []]
+def create_patient_entry(first_name, last_name, patient_mrn, patient_age):
+    #new_patient = [patient_name, patient_mrn, patient_age, []]
+    new_patient = {'First Name': first_name, "Last Name": last_name, 
+                   "MRN": patient_mrn, "Age": patient_age, 
+                   "Tests": []}
     return new_patient
 
 
 def main_driver():
     db = []
-    db.append(create_patient_entry("Ann Ables", 1, 34))
-    db.append(create_patient_entry("Bob Boyles", 2, 45))
-    db.append(create_patient_entry("Chris Chou", 3, 52))
+    db.append(create_patient_entry("Ann", "Ables", 1, 34))
+    db.append(create_patient_entry("Bob", "Boyles", 2, 45))
+    db.append(create_patient_entry("Chris", "Chou", 3, 52))
     print(db)
     add_test_to_patient(db, 1, "HDL", 120)
     add_test_to_patient(db, 2, "LDL", 100)
